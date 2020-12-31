ea066770d93d15de3bf2fcf2956e785aaa287193
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-12-31 09:54:34 +0700

    Update gatsby configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,43 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    siteMetadata: {
-        title: `Novela by Narative`,
-        name: `Narative`,
-        siteUrl: `https://gatsby-theme-novela.netlify.com`,
-        description: `This is my description that will be used in the meta tags and important for search results`,
+  siteMetadata: {
+    title: `Tranity Project Blog`,
+    name: `Tranity Project Blog`,
+    siteUrl: `https://blog.tranityproject.com`,
+    description: `Tranity Project is an open source community that help people to make their own application, web or mobile.`,
 
-        // important to set the main text that appears in the hero
-        hero: {
-            heading: `Perspectives on technology, design and business from the team at Narative.`,
-            maxWidth: 652,
-        },
-        social: [
-            {
-                name: `twitter`,
-                url: `https://twitter.com/narative`,
-            },
-            {
-                name: `github`,
-                url: `https://github.com/narative`,
-            },
-        ],
+    // important to set the main text that appears in the hero
+    hero: {
+      heading: `Perspectives on technology, design and business from the team at Tranity Project.`,
+      maxWidth: 652,
     },
-    plugins: [
-        {
-            resolve: "@narative/gatsby-theme-novela",
-            options: {
-                contentPosts: "content/posts",
-                contentAuthors: "content/authors",
-                rootPath: "/",
-                basePath: "/",
-                authorsPage: true,
-                mailchimp: false,
-                sources: {
-                    local: true,
-                    contentful: false,
-                },
-            },
-        }
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/tranityproject`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/tranityproject`,
+      },
     ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        rootPath: "/",
+        basePath: "/",
+        authorsPage: true,
+        mailchimp: false,
+        sources: {
+          local: true,
+          contentful: false,
+        },
+      },
+    },
+  ],
 };
